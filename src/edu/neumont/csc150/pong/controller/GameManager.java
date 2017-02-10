1361973a8edd07c5694dab3bb3a1cdbb634f8759
Michael Brannen <MBrannen@student.neumont.edu>
2017-02-10 10:42:18 -0700

    PlayGame

--- src/edu/neumont/csc150/pong/controller/GameManager.java
+++ src/edu/neumont/csc150/pong/controller/GameManager.java
@@ -2,4 +2,9 @@ package edu.neumont.csc150.pong.controller;
 
 public class GameManager {
 
+	public void run() {
+	
+		
+	}
+	
 }
--- src/edu/neumont/csc150/pong/controller/PlayGame.java
+++ src/edu/neumont/csc150/pong/controller/PlayGame.java
@@ -3,8 +3,8 @@ package edu.neumont.csc150.pong.controller;
 public class PlayGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GameManager game = new GameManager();
+		game.run();
 	}
 
 }
