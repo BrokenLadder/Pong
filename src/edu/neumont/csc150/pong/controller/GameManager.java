2d2adb3612c2bf35633b90eaaf9244b475652673
Michael Brannen <MBrannen@student.neumont.edu>
2017-02-13 10:15:18 -0700

    sdfsdfsdf

--- src/edu/neumont/csc150/pong/controller/GameManager.java
+++ src/edu/neumont/csc150/pong/controller/GameManager.java
@@ -1,10 +1,12 @@
 package edu.neumont.csc150.pong.controller;
 
+import edu.neumont.csc150.pong.view.GUI;
+
 public class GameManager {
 
 	public void run() {
-	
-		
+		GUI gui = new GUI(this);
+		gui.initGui();
 	}
 	public void isWon(){
 		
--- src/edu/neumont/csc150/pong/view/GUI.java
+++ src/edu/neumont/csc150/pong/view/GUI.java
@@ -2,7 +2,19 @@ package edu.neumont.csc150.pong.view;
 
 import javax.swing.JFrame;
 
+import edu.neumont.csc150.pong.controller.GameManager;
+
 public class GUI {
+	GameManager game;
 	private JFrame window;
+	public GUI(GameManager gameManager) {
+		game = gameManager;
+	}
+	public void initGui() {
+		
+		
+	}
+
+	
 	
 }
