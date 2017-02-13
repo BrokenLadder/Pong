b929274010c4635651970037618328b7fa179d1b
Gabe Barber <GBarber@student.neumont.edu>
2017-02-13 10:15:51 -0700

    fdsa

--- src/edu/neumont/csc150/pong/controller/Collision.java
+++ src/edu/neumont/csc150/pong/controller/Collision.java
@@ -1,5 +1,13 @@
 package edu.neumont.csc150.pong.controller;
 
 public class Collision {
-
+	public void nonScoreCollision(){
+		
+	}
+	public void scoreCollision(){
+		
+	}
+	public void isCollision(){
+		
+	}
 }
--- src/edu/neumont/csc150/pong/model/Paddle.java
+++ src/edu/neumont/csc150/pong/model/Paddle.java
@@ -1,5 +1,14 @@
 package edu.neumont.csc150.pong.model;
 
+
 public class Paddle {
+int width;
+int yVelocity;
+int height;
+int xVelocity;
+int yPosition;
+int xPosition;
+
+
 
 }
