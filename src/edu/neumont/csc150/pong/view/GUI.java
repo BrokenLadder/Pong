475aa908dee2d4a79a5b104dedb4a2476e3fd2bf
Gabe Barber <GBarber@student.neumont.edu>
2017-02-14 09:34:48 -0700

    dsa

--- src/edu/neumont/csc150/pong/view/GUI.java
+++ src/edu/neumont/csc150/pong/view/GUI.java
@@ -21,10 +21,12 @@ public class GUI implements ActionListener{
 	private JMenuBar menuBar;
 	private JMenuItem newGame,exit,theme;
 	private Drawing drawingPanel;
-	private Font gameFont = new Font("Arial", Font.PLAIN,0);
+	private Font gameFont = new Font("Helvetica", Font.BOLD, 50);
+	
 	public GUI(GameManager gameManager) {
 		game = gameManager;
 	}
+	
 	public void initGUI() {
 		window = new JFrame("Pong");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,7 +35,9 @@ public class GUI implements ActionListener{
 		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		window.getContentPane().add(mainPanel);
 		window.setVisible(true);
+		
 	}
+	
 	private void loadGUI() {
 		initializeItems();
 		createMenu();
@@ -47,37 +51,47 @@ public class GUI implements ActionListener{
 	private void initializeItems() { //TODO add to uml
 		drawingPanel = new Drawing();
 		mainPanel = new JPanel();
-		menu = new JMenu();
+		menu = new JMenu("Game Menu");
 		menuBar = new JMenuBar();
-		newGame = new JMenuItem();
-		exit = new JMenuItem();
-		theme = new JMenuItem();
+		newGame = new JMenuItem("New Game");
+		exit = new JMenuItem("Exit");
+		theme = new JMenuItem("Theme");
+		
+		newGame.setFont(gameFont);
+		exit.setFont(gameFont);
+		theme.setFont(gameFont);
 	}
 	private void createMenu() { //TODO add to uml
 		menu.add(newGame);
 		menu.add(theme);
 		menu.add(exit);
 		menuBar.add(menu);
+		window.setJMenuBar(menuBar);
+		
 	}
 	private void setLayouts() {
 		mainPanel.setLayout(new BorderLayout());
 	}
+	
 	private void addListeners() {
 		newGame.addActionListener(this);
 		theme.addActionListener(this);
 		exit.addActionListener(this);
 	}
+	
 	private void addPanels() {
+		window.add(menuBar);
 		window.add(mainPanel);
-		mainPanel.add(menuBar,BorderLayout.NORTH);
 		mainPanel.add(drawingPanel,BorderLayout.CENTER);
 	}
 	
 	private void setGUIFont() {
 		mainPanel.setFont(gameFont);
 		menuBar.setFont(gameFont);
+		menu.setFont(gameFont);
 		
 	}
+	
 	private void setGUIMargins() {
 		// TODO Auto-generated method stub
 		
