d16b19fbd1fbf7817e8c049a35ffa6853a02ee2e
Michael Brannen <MBrannen@student.neumont.edu>
2017-02-14 09:39:24 -0700

    gsdgsdgsdgsdgsdgsdg

--- src/edu/neumont/csc150/pong/view/GUI.java
+++ src/edu/neumont/csc150/pong/view/GUI.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -13,7 +15,7 @@ import javax.swing.JPanel;
 
 import edu.neumont.csc150.pong.controller.GameManager;
 
-public class GUI implements ActionListener{
+public class GUI implements ActionListener, KeyListener{
 	private GameManager game;
 	private JFrame window;
 	private JPanel mainPanel;
@@ -106,6 +108,24 @@ public class GUI implements ActionListener{
 		
 	}
 	public void resetGUI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
