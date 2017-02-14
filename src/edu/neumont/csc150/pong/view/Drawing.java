e5cbd74f4405be794f97c151ad989dcae2c927dd
Michael Brannen <MBrannen@student.neumont.edu>
2017-02-14 09:03:13 -0700

    sd;

---
+++ src/edu/neumont/csc150/pong/view/Drawing.java
@@ -0,0 +1,16 @@
+package edu.neumont.csc150.pong.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class Drawing extends JPanel implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
