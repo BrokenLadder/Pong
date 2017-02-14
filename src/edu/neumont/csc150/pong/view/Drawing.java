1b3b7b92de84b6d2e420d5e7d2fae99d21af9b41
Michael Brannen <MBrannen@student.neumont.edu>
2017-02-14 09:52:28 -0700

    ijiji

--- src/edu/neumont/csc150/pong/view/Drawing.java
+++ src/edu/neumont/csc150/pong/view/Drawing.java
@@ -1,16 +1,40 @@
 package edu.neumont.csc150.pong.view;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import edu.neumont.csc150.pong.model.Ping;
+
+
 
 public class Drawing extends JPanel implements ActionListener {
-
+	private Timer timer = new Timer((2),this);
+	public Ping ping;
+	public Drawing(Ping ball) {
+		this.ping = ball;
+		this.setBackground(Color.darkGray);
+	}
+	@Override
+	public void paint(Graphics g) {
+		g.fillRect(ping.getxPosition(), ping.getyPosition(), ping.getWidth(), ping.getHeight());
+		super.paint(g);
+		
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(e.getSource() == this.timer) {
+			if (ping != null) {
+				ping.setxPosition(ping.getxPosition() + ping.getxVelocity());
+				ping.setyPosition(ping.getyPosition() + ping.getyVelocity());
+				//ping.detectCollision(this.getWidth(), this.getHeight(), 0, 0);
+			}
+		} 
+		this.repaint();
 	}
 
 }
