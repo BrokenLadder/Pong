ebcd345cbcadb867ae8217f19e84e6773855d060
Gabe Barber <GBarber@student.neumont.edu>
2017-02-14 09:52:04 -0700

    keyStroke

--- src/edu/neumont/csc150/pong/view/GUI.java
+++ src/edu/neumont/csc150/pong/view/GUI.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+import javax.swing.JOptionPane;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -112,9 +112,14 @@ public class GUI implements ActionListener, KeyListener{
 		
 	}
 
+	
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		int key = e.getKeyCode();
+		if(key == KeyEvent.VK_DOWN){
+			//JOptionPane
+		}
 		
 	}
 
