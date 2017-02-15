18bd76c9051c7cd20b183c1ae0b66f7f172ca42c
Gabe Barber <GBarber@student.neumont.edu>
2017-02-15 10:24:16 -0700

    yolo

--- src/edu/neumont/csc150/pong/view/Drawing.java
+++ src/edu/neumont/csc150/pong/view/Drawing.java
@@ -4,18 +4,24 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
 import edu.neumont.csc150.pong.controller.GameManager;
 
-public class Drawing extends JPanel {
+public class Drawing extends JPanel implements ActionListener {
 	private static final long serialVersionUID = -7398355064246963722L;
 	private GameManager game;
 	private BufferedImage image;
+	private Timer timer = new Timer(2, this);
+	
 	public Drawing(GameManager game) throws IOException {
 		this.game = game;
 		image = ImageIO.read(new File("pong_default2.png"));
@@ -32,8 +38,22 @@ public class Drawing extends JPanel {
 				   game.getPaddle1().getWidth(), game.getPaddle1().getHeight());
 		g.fillRect(game.getPaddle2().getxPosition(), game.getPaddle2().getyPosition(), 
 				   game.getPaddle2().getWidth(), game.getPaddle2().getHeight());
+	
 	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == this.timer){
+			game.getBall().setxPosition(game.getBall().getxPosition() + game.getBall().getxVelocity());;
+			game.getBall().setyPosition(game.getBall().getyPosition() + game.getBall().getyVelocity());;
 
+		}
+		this.repaint();
+		
+	}
+	public void startTimer(){
+		this.timer.start();
+	}
 
 	
 }
